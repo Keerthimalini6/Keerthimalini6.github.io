402f246bb9e13493f9823322ee819bb5aa76b352
Keerthimalini6 <keerthimalini6699@gmail.com>
2023-03-31 17:36:40 +0530

    home page

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   images: {
-    loader: 'akamai',
-    path: '',
+    unoptimized: true,
   },
 }
-
-module.exports = nextConfig
